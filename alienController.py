bc3b5beeff0ea8ca4409bc72c9297f9ff95566b8
IliaIlinich <ilya.ilinich@gmail.com>
2025-10-14 15:37:52 +0100

    Comments added to code

--- alienController.py
+++ alienController.py
@@ -1,10 +1,12 @@
 import pygame
 
+# General alien class
 class Alien:
     def __init__(self, pos_x, pos_y):
         self.position = pygame.Vector2(pos_x, pos_y)
 
 screen = pygame.display.set_mode((1280, 720))
 
+# (Temporary) function to draw an alien
 def draw_alien(alien_in):
     pygame.draw.circle(screen, "red", alien_in.position, 30)
--- main.py
+++ main.py
@@ -1,43 +1,50 @@
 import pygame
 import alienController as ALC
 
+# Initialize Pygame
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+# Player setup
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-alien1 = ALC.Alien(screen.get_width() / 2, screen.get_height() / 2)
+# Alien setup
+alien1 = ALC.Alien(screen.get_width() / 3, screen.get_height() / 3)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    #background
-    screen.fill("black")
+# Main loop
+if __name__ == "__main__":
+    while running:
+        # Check if the game was closed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # Background setup
+        screen.fill("black")
 
-    #player
-    pygame.draw.circle(screen, "green", player_pos, 40)
+        # Draw player
+        pygame.draw.circle(screen, "green", player_pos, 40)
 
-    # basic movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player_pos.x -= 10
-    if keys[pygame.K_RIGHT]:
-        player_pos.x += 10
-    if keys[pygame.K_UP]:
-        player_pos.y -= 10
-    if keys[pygame.K_DOWN]:
-        player_pos.y += 10
+        # Basic player movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player_pos.x -= 10
+        if keys[pygame.K_RIGHT]:
+            player_pos.x += 10
+        if keys[pygame.K_UP]:
+            player_pos.y -= 10
+        if keys[pygame.K_DOWN]:
+            player_pos.y += 10
 
-    # Aliens
-    ALC.draw_alien(alien1)
+        # Draw alien
+        ALC.draw_alien(alien1)
 
-    pygame.display.flip()
+        # Update the display
+        pygame.display.flip()
 
-    # 60 fps
-    clock.tick(60)
+        # The game is fixed to 60 fps
+        clock.tick(60)
 
-
+# Quit Pygame
 pygame.quit()
