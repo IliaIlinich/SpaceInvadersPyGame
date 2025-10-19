f1b4832ee8bcd6a94555e5d59a5ee024244f9520
IliaIlinich <ilya.ilinich@gmail.com>
2025-10-19 16:10:42 +0100

    Alien movement testing added

--- alienController.py
+++ alienController.py
@@ -4,6 +4,21 @@ import pygame
 class Alien:
     def __init__(self, pos_x, pos_y):
         self.position = pygame.Vector2(pos_x, pos_y)
+        self.stage = 0 # Terminal variable for movement testing
+        self.timer_move = pygame.USEREVENT + 1
 
     def draw(self, surface):
         pygame.draw.circle(surface, "red", self.position, 30)
+
+    # Movement
+    def move_right(self):
+        self.position.x += 60
+    
+    def move_left(self):
+        self.position.x -= 60
+    
+    def move_up(self):
+        self.position.y -= 60
+
+    def move_down(self):
+        self.position.y += 60
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 # Alien setup
 alien1 = ALC.Alien(screen.get_width() / 3, screen.get_height() / 3)
+pygame.time.set_timer(alien1.timer_move, 1000)  # Alien movement timer
 
 # Main loop
 if __name__ == "__main__":
@@ -20,31 +21,47 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        # Background setup
-        screen.fill("black")
+            else:
+                # Background setup
+                screen.fill("black")
 
-        # Draw player
-        pygame.draw.circle(screen, "green", player_pos, 40)
+                # Draw player
+                pygame.draw.circle(screen, "green", player_pos, 40)
 
-        # (Temporary) Basic player movement
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            player_pos.x -= 10
-        if keys[pygame.K_RIGHT]:
-            player_pos.x += 10
-        if keys[pygame.K_UP]:
-            player_pos.y -= 10
-        if keys[pygame.K_DOWN]:
-            player_pos.y += 10
+                # (Temporary) Basic player movement
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_LEFT]:
+                    player_pos.x -= 10
+                if keys[pygame.K_RIGHT]:
+                    player_pos.x += 10
+                if keys[pygame.K_UP]:
+                    player_pos.y -= 10
+                if keys[pygame.K_DOWN]:
+                    player_pos.y += 10
 
-        # Draw alien
-        alien1.draw(screen)
+                # Movement test
+                if event.type == alien1.timer_move:
+                    if alien1.stage == 0:
+                        alien1.move_right()
+                        alien1.stage += 1
+                    elif alien1.stage == 1:
+                        alien1.move_down()
+                        alien1.stage += 1
+                    elif alien1.stage == 2:
+                        alien1.move_left()
+                        alien1.stage += 1
+                    elif alien1.stage == 3:
+                        alien1.move_up()
+                        alien1.stage = 0
 
-        # Update the display
-        pygame.display.flip()
+                # Draw alien
+                alien1.draw(screen)
 
-        # The game is fixed to 60 fps
-        clock.tick(60)
+                # Update the display
+                pygame.display.flip()
+
+                # The game is fixed to 60 fps
+                clock.tick(60)
 
 # Quit Pygame
 pygame.quit()
