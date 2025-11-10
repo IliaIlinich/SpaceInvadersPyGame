a2f5c33fa61f51d2f9d59a5e0b2daed47de6ffb5
IliaIlinich <ilya.ilinich@gmail.com>
2025-11-10 13:32:55 +0000

    Alien sprites added, minor changes to alien movement, bullets now can be fired randomly by 1 to 3 aliens

--- alienController.py
+++ alienController.py
@@ -7,20 +7,18 @@ class Alien:
         self.position = pygame.Vector2(pos_x, pos_y)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "red", self.position, 30)
+        #pygame.draw.circle(screen, "red", self.position, 30)
+        i = 0 # just some code so no error
 
     def get_position(self):
         return self.position
 
     # Movement
     def move_right(self):
-        self.position.x += 60
+        self.position.x += 10
     
     def move_left(self):
-        self.position.x -= 60
-    
-    def move_up(self):
-        self.position.y -= 60
+        self.position.x -= 10
 
     def move_down(self):
         self.position.y += 60
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import alienController as ALC
 import alienLevels as ALL
 from playerControler import Player as PC
 
+
 # Initialize Pygame
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -19,13 +20,24 @@ level1 = ALL.level(screen, 0)
 # Aliens & Bullets
 bullets = []
 ALIEN_SHOOT_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(ALIEN_SHOOT_EVENT, 1000 - level1.difficulty * 100)  # Adjust shooting frequency based on difficulty
+pygame.time.set_timer(ALIEN_SHOOT_EVENT, 3000 - level1.difficulty * 100)  # Adjust shooting frequency based on difficulty
 # FIX LATER, so at some point the timer is not less than 0!!!!!!!!
 
 # Alien movement event
 ALIEN_MOVE_EVENT = pygame.USEREVENT + 2
-pygame.time.set_timer(ALIEN_MOVE_EVENT, 1000)
+pygame.time.set_timer(ALIEN_MOVE_EVENT, 600)
 current_movement = "right"
+
+# Fix later
+alien_sprites = [pygame.image.load("./Sprites/Aliens/alien_1_sprite_1.png"),
+                pygame.image.load("./Sprites/Aliens/alien_1_sprite_2.png")]
+alien_sprites[0] = pygame.transform.scale(alien_sprites[0], (70, 70))
+alien_sprites[1] = pygame.transform.scale(alien_sprites[1], (70, 70))
+
+animation_iter = 0
+ALIEN_ANIMATION_EVENT = pygame.USEREVENT + 3
+pygame.time.set_timer(ALIEN_ANIMATION_EVENT, 600)
+
 
 # Main loop
 if __name__ == "__main__":
@@ -34,23 +46,28 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # Check for quitting the game
                 running = False
+            elif event.type == ALIEN_ANIMATION_EVENT: # Event for alien animation
+                animation_iter += 1
+                if animation_iter >= len(alien_sprites):
+                    animation_iter = 0
             elif event.type == ALIEN_SHOOT_EVENT: # Event for bullets update
-                cnt = 0 # Counter to find the correct alien to shoot
-                alien_index = random.randint(0, 54)
-                alien_found = False
-                for row in level1.alien_list:
-                    if alien_found:
-                        break
-                    for alien in row:
-                        if cnt == alien_index:
-                            alien.shoot(screen, bullets)
-                            alien_found = True
+                for i in range(random.randint(1, 3)):
+                    cnt = 0 # Counter to find the correct alien to shoot
+                    alien_index = random.randint(0, 54)
+                    alien_found = False
+                    for row in level1.alien_list:
+                        if alien_found:
                             break
-                        else:
-                            cnt += 1
-                for bullet in bullets:
-                    if bullet.position.y > 720:
-                        bullets.remove(bullet)
+                        for alien in row:
+                            if cnt == alien_index:
+                                alien.shoot(screen, bullets)
+                                alien_found = True
+                                break
+                            else:
+                                cnt += 1
+                    for bullet in bullets:
+                        if bullet.position.y > 720:
+                            bullets.remove(bullet)
             elif event.type == ALIEN_MOVE_EVENT: # Event for alien movement
                 if current_movement == "right":
                     edge_reached = False
@@ -102,6 +119,10 @@ if __name__ == "__main__":
         # Draw level
         level1.draw_level(screen)
 
+        # Draw Aliens
+        for row in level1.alien_list:
+            for alien in row:
+                screen.blit(alien_sprites[animation_iter], (alien.get_position().x, alien.get_position().y))
 
         # Bullets
         for bullet in bullets:
