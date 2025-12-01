31601a4e169a779431b7a2e3ba053424685c5cc1
Harrison <hjs.king@icloud.com>
2025-12-01 10:05:38 +0000

    added shield sprites, started sprite updates for shields

--- shieldController.py
+++ shieldController.py
@@ -1,5 +1,10 @@
 import pygame 
 
+shield_sprites =[["lt",[1,2,3,4]],
+                 ["lb",[1,2,3,4]],
+                 ["rt",[1,2,3,4]],
+                 ["rb",[1,2,3,4]],
+                 ["nm",[1,2,3,4]]]
 
 class Base:
     def __init__(self):
@@ -7,12 +12,26 @@ class Base:
                        [["3",4],["lb",4],["rb",4],["4",4]]
                        [["5",4],["6",0],["7",0],["8",0]]]
         self.damaged_tile = ""
+        self.numbers = ["1","2","3","4","5","6","7","8"]
+        self.current_space = ""
     
     def update_damage(self):
         for i in range(0,3):
             for j in range(0,4):
                 if self.damaged_tile == self.shield[i][j][0]:
                     self.shield[i][j][1] -= 1
+
+    def update_sprite(self, shield_sprites):
+        if self.damaged_tile == "":
+            looping = True
+            while looping:
+                for i in range(0,3):
+                    for j in range(0,4):
+                        self.current_space = self.shield[i][j][0]
+                        
+
+
+
 
     
                     
