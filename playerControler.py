49cd7977313e415193d2e955ea77da7353c27b0e
Caleb Boonaert <caleb.boonaert@gmail.com>
2025-12-02 09:34:49 +0000

    fixed bug where the player could move off the screen

--- playerControler.py
+++ playerControler.py
@@ -5,6 +5,7 @@ class Player:
     def __init__(self, screen):
         self.screen = screen
         self.player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 1.1)
+        self.player_bounds = [50, screen.get_width()-50]
         self.player_rect = ""
         self.shoot_cooldown = 0
         self.lives = 4
@@ -15,9 +16,11 @@ class Player:
         keys = pygame.key.get_pressed()
         speed = 5
         if keys[pygame.K_LEFT]:
-            self.player_pos.x -= speed
+            if self.player_pos.x - speed > self.player_bounds[0]:
+                self.player_pos.x -= speed
         if keys[pygame.K_RIGHT]:
-            self.player_pos.x += speed
+            if self.player_pos.x + speed < self.player_bounds[1]:
+                self.player_pos.x += speed
         if keys[pygame.K_SPACE]:
             if self.shoot_cooldown <= 0:
                 self.shoot_cooldown = 40
